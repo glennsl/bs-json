101a88829dc6480202b10f6ee14c6f822d0d1b54
glennsl <glenn@slotte.net>
2017-05-03 18:11:15 +0200

    Added encode tests

---
+++ __tests__/json_encode_test.ml
@@ -0,0 +1,40 @@
+open Jest
+open Expect
+open! Json.Encode
+
+let _ =
+
+test "null" (fun () ->
+  expect null |> toEqual @@ Obj.magic Js.null);
+
+test "string" (fun () ->
+  expect @@ string "foo" |> toEqual @@ Obj.magic "foo");
+
+test "float" (fun () ->
+  expect @@ float 1.23 |> toEqual @@ Obj.magic 1.23);
+
+test "int" (fun () ->
+  expect @@ int 23 |> toEqual @@ Obj.magic 23);
+
+test "boolean" (fun () ->
+  expect @@ boolean Js.true_ |> toEqual @@ Obj.magic Js.true_);
+
+test "object_ - empty" (fun () ->
+  expect @@ object_ @@ Js.Dict.empty () |> toEqual @@ Obj.magic @@ Js.Dict.empty ());
+
+test "object_ - simple" (fun () ->
+  let o = Js.Dict.empty () in
+  Js.Dict.set o "x" (int 42);
+  expect @@ object_ o |> toEqual @@ Obj.magic o);
+
+test "array int" (fun () ->
+  expect @@ array ([|1;2;3|] |> Array.map int) |> toEqual @@ Obj.magic [|1;2;3|]);
+
+test "stringArray" (fun () ->
+  expect @@ stringArray [|"a";"b"|]  |> toEqual @@ Obj.magic [|"a";"b"|]);
+
+test "nubmerArray" (fun () ->
+  expect @@ numberArray [|0.;4.|] |> toEqual @@ Obj.magic [|0;4|]);
+
+test "booleanArray" (fun () ->
+  expect @@ booleanArray [|Js.true_;Js.false_|] |> toEqual @@ Obj.magic [|Js.true_;Js.false_|]);
