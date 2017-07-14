a45352ab76ed5a822f39a043b27ebf086a70c833
glennsl <glenn@slotte.net>
2017-07-14 22:15:14 +0200

    add advanced Encode.object_ example

--- examples/encode.ml
+++ examples/encode.ml
@@ -24,3 +24,43 @@ let _ =
     ]
     |> Js.log
   )
+
+(* Advanced example: encode a record *)
+type line = {
+  start: point;
+  end_: point;
+  thickness: int option
+}
+and point = {
+  x: float;
+  y: float
+}
+
+module Encode = struct
+  let point r =
+    let open! Json.Encode in (
+      object_ [
+        ("x", float r.x);
+        ("y", float r.y)
+      ]
+    )
+  let line r =
+    Json.Encode.(
+      object_ [
+        ("start", point r.start);
+        ("end", point r.end_);
+        ("thickness", match r.thickness with Some x -> int x | None -> null)
+      ]
+    )
+end
+
+let data = {
+  start = { x = 1.1; y = -0.4 };
+  end_ = { x = 5.3; y = 3.8 };
+  thickness = Some 2
+}
+
+let _ =
+  data
+  |> Encode.line
+  |> Js.log
